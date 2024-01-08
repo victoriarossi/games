0c229fa78c7d3fadef38c5e2e064405b95397cd5
victoriarossi <vrossi@itba.edu.ar>
2024-01-07 19:29:25 -0600

    one or two player impl + css

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} /> 
+          <Route path="/" element={<Home />} /> 
           <Route path="/tictactoe" element={<TicTacToe />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
--- src/pages/TicTacToe.js
+++ src/pages/TicTacToe.js
@@ -1,39 +1,93 @@
 import React, { useState } from 'react';
 import Header from '../components/Header';
 import '../css/TicTacToe.css';
+import { Link } from "react-router-dom";
 
 export default function TicTacToe() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [players, setPlayers] = useState(2);
+  let end = false;
 
   function makeMove(id) {
     if (!board[id] && !calculateWinner(board)) {
       const newBoard = [...board];
       newBoard[id] = currentPlayer;
       setBoard(newBoard);
-      setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+      if(players === 2){
+        setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+      } else {
+        makeMachineMove(newBoard);
+      }
+    }
+  }
+
+  function makeMachineMove(board) { // TODO: improve AI
+    if (players === 1) {
+      const newBoard = [...board];
+      let id = Math.floor(Math.random() * 9);
+      while (newBoard[id]) {
+        id = Math.floor(Math.random() * 9);
+      }
+      newBoard[id] = 'O';
+      setBoard(newBoard);
     }
   }
 
   function renderSquare(id) {
     return (
-      <button className="square" onClick={() => makeMove(id)}>
+      <button className="square" onClick={() =>{ 
+        if(players === 2 || (players === 1 && currentPlayer === 'X')){
+          makeMove(id)
+        }else{
+          alert("It's not your turn")
+        }
+      }}>
         {board[id]}
       </button>
     );
   }
 
+  function setGame(players) {
+    
+  }
+
   const winner = calculateWinner(board);
   let status;
+  let gameover;
   if (winner) {
-    status = `Winner: ${winner}`;
+    status = " ";
+    end = true;
+  gameover = (
+    <React.Fragment>
+      Game Over <br />
+      Winner: {winner}!!!
+    </React.Fragment>
+  );
+  } else if (!board.includes(null)) {
+    status = ' ';
+    end = true;
+    gameover = (
+      <React.Fragment>
+        Game Over <br />
+        Draw!!!
+      </React.Fragment>
+    );
   } else {
     status = `Next player: ${currentPlayer}`;
   }
 
   return (
-    <>
+    <div>
+      <Link to="/" className='no-underline'>
+        <a class="btn disabled back_btn"> <i class="material-icons">arrow_back</i></a>
+      </Link>
       <Header name="Tic Tac Toe" />
+      <div className='select-game'>
+        <div>Select one game:</div>
+        <button className='players-btn' onClick={() => setPlayers(1)}>One player</button>
+        <button className='players-btn' onClick={() => setPlayers(2)}>Two players</button>
+      </div>      
       <div className="status">{status}</div>
       <div className="board">
         <div className="board-col">
@@ -53,8 +107,11 @@ export default function TicTacToe() {
             {renderSquare(8)}
           </div>
         </div>
+        <div>
+        {end && <h1 className="game-over">{gameover}</h1>}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
